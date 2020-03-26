82217bd72316b28423770cd2d9450674b3d84cbb
DaMota-daniel <da-mota.daniel@lyceeppr.fr>
2020-03-26 12:42:16 +0100

    setter getter

--- src/classes/etudiant.h
+++ src/classes/etudiant.h
@@ -3,13 +3,26 @@ class etudiant
 public:
 
 //Constructeur par défault
-nomEtudiant = 
+nomEtudiant;
+
+int setNom
+
+int getNom;
+
+int setnote1;
+
+int getnote1;
+
+int setnote2,
+
+int getnote2;
 
 
 
 private;
 char nomEtudiant;
-
-
+int note1;
+int note2;
+int Nom;
 
 };
